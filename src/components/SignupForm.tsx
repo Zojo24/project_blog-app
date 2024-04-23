d45ef3b3a47857049e1e2f6f9df5e05397badaca
Zojo <hchoi2894@gmail.com>
2024-04-23 12:32:58 +0900

    add: 로그인/회원가입 폼 추가

--- src/components/SignupForm.tsx
+++ src/components/SignupForm.tsx
@@ -1,9 +1,9 @@
 import { Link } from "react-router-dom";
 
-export default function LoginForm() {
+export default function SignupForm() {
   return (
     <form action="/post" method="POST" className="form form-lg">
-      <h1 className="form_title">로그인</h1>
+      <h1 className="form_title">회원가입</h1>
       <div className="form_block">
         <label htmlFor="email">이메일</label>
         <input type="email" name="email" id="email" required />
@@ -13,13 +13,22 @@ export default function LoginForm() {
         <input type="password" name="password" id="password" required />
       </div>
       <div className="form_block">
-        계정이 없으신가요?{" "}
+        <label htmlFor="password_confirm">비밀번호 확인</label>
+        <input
+          type="password_confirm"
+          name="password_confirm"
+          id="password_confirm"
+          required
+        />
+      </div>
+      <div className="form_block">
+        계정이 이미 있으신가요?{" "}
         <Link to="/signup" className="form_link">
-          회원가입하기
+          로그인하기
         </Link>
       </div>
       <div className="form_block">
-        <input type="submit" value="로그인" className="form_btn-submit" />
+        <input type="submit" value="회원가입" className="form_btn-submit" />
       </div>
     </form>
   );
