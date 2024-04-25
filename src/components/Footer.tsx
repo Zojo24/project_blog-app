cb85161983eb88f30f27226ce9d16e622d8eac4c
Zojo <hchoi2894@gmail.com>
2024-04-25 17:02:03 +0900

    edit: footer 간격 수정

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -7,11 +7,9 @@ export default function Footer() {
   const context = useContext(ThemeContext);
   return (
     <footer>
-      <div>
-        <Link to="/posts/new">글쓰기</Link>
-        <Link to="/posts">게시글</Link>
-        <Link to="/profile">프로필</Link>
-      </div>
+      <Link to="/posts/new">글쓰기</Link>
+      <Link to="/posts">게시글</Link>
+      <Link to="/profile">프로필</Link>
       <>
         {context.theme === "light" ? (
           <BsSun onClick={context.toggleMode} className="footer_theme-btn" />
